e0f31ed63b4e339e841da5cddc755c330dc6ffdd
Ramsey McGrath <Ramsey_McGrath@symantec.com>
2017-10-31 00:45:37 -0700

    More pathing weirdness

--- gradingApplication/start_grading.sh
+++ gradingApplication/start_grading.sh
@@ -3,14 +3,14 @@ pushd user_data
 NAMES=`grep -v '^#' user_config.conf` #read name(s) from user_config.conf file
 PROJECTS=`grep -v '^#' projects_config.conf` #read projects(s) from projects_config.conf file
 
-mkdir credentials
+mkdir ../credentials
 
 for NAME in $NAMES; do
    for PROJECT in $PROJECTS; do
       echo "Adding credentials to credentials folder"
-      cp projects/$PROJECT/credentials.ini credentials/$NAME-$PROJECT-credentials.ini
+      cp projects/$PROJECT/credentials.ini ../credentials/$NAME-$PROJECT-credentials.ini
    done
    echo "Grading projects for $NAME"
-   bash grade.sh $NAME
+   bash ../grade.sh $NAME
 done
 popd
