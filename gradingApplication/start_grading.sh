618036dc0bd72d4ae57be65c1013490bd4078882
Ramsey McGrath <Ramsey_McGrath@symantec.com>
2017-10-31 00:41:52 -0700

    Some pathing errors from the refactoring

--- gradingApplication/start_grading.sh
+++ gradingApplication/start_grading.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+pushd user_data
 NAMES=`grep -v '^#' user_config.conf` #read name(s) from user_config.conf file
 PROJECTS=`grep -v '^#' projects_config.conf` #read projects(s) from projects_config.conf file
 
@@ -7,7 +8,7 @@ mkdir credentials
 for NAME in $NAMES; do
    for PROJECT in $PROJECTS; do
       echo "Adding credentials to credentials folder"
-      cp user_data/projects/$PROJECT/credentials.ini credentials/$NAME-$PROJECT-credentials.ini
+      cp projects/$PROJECT/credentials.ini credentials/$NAME-$PROJECT-credentials.ini
    done
    echo "Grading projects for $NAME"
    bash grade.sh $NAME
